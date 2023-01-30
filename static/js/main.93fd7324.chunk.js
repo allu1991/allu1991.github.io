730f240b6367310e8029a6f99602facce69c8dc9
Aleksi Pitkanen <aleksi.pitkanen@dqcomms.com>
2023-01-30 14:29:09 +0200

    Updates

--- static/js/main.93fd7324.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var o=n(0),c=(n(1),n(4)),r=n.n(c);n(3);var i=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("p",{children:"Site moved"})})};r.a.render(Object(o.jsx)(i,{}),document.getElementById("root"))},3:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.93fd7324.chunk.js.map
